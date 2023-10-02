0ea6e21ea4de93fca6adbd833de6ee809a8a0d34
niladri <niladri.stud@gmail.com>
2023-10-03 00:39:12 +0530

    Text change Header file

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -4,8 +4,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">BLOG</span>
+        <span className="headerTitleSm">Welcome to </span>
+        <span className="headerTitleLg">OmniBlog</span>
       </div>
       <img
         className="headerImg"
